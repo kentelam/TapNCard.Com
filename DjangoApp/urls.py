8e2405ed5e938698f2da8ac61eebb88db1898a8a
kentelam <kentelam8@gmail.com>
2023-05-24 23:07:57 -0700

    members local push

--- DjangoApp/urls.py
+++ DjangoApp/urls.py
@@ -15,9 +15,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    
+    path("admin/", admin.site.urls),
 
-    path('', include('main.urls'))
-]
+    path("", include('main.urls')),
+
+    path('members/', include('django.contrib.auth.urls')),
+
+    path('members', include('members.urls')),
+
+
+
+
+]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
---
+++ members/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import UserRegisterView
+
+
+urlpatterns = [
+    
+path('register/', UserRegisterView.as_view(), name='register'),
+
+
+
+    
+    
+
+]
